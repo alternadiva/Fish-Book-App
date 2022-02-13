abe44e12d79cb1201dec7d2cc40f7cbeac8f0db1
alternadiva <asomeonelse@gmail.com>
2022-02-13 21:44:13 +0100

    add event listener to form

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let form = document.getElementById("form");
+let inputValue = document.getElementById("country-input");
+let submitBtn = document.getElementById("submit-search");
+let randomBtn = document.getElementById("random-btn");
+let output = document.getElementById("output");
+
+form.addEventListener("submit", getCountry);
+
+
 fetch("https://restcountries.com/v3.1/all")
       .then((response) => {
         if (!response.ok) {
