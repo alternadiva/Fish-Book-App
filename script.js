c017377e7a15f2ca49807b9c2cb3ffbfcb1a5a8a
alternadiva <asomeonelse@gmail.com>
2022-02-14 00:46:00 +0100

    log search input results

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ let form = document.getElementById("form");
 let inputValue = document.getElementById("country-input");
 let submitBtn = document.getElementById("submit-search");
 let randomBtn = document.getElementById("random-btn");
+let resultsList = document.getElementById("results-list");
 let output = document.getElementById("output");
 
+// Submit input and show result
+
 form.addEventListener("submit", getCountry);
+
 
 function getCountry(event) {
   event.preventDefault();
 
   let formData = new FormData(form);
-  let value = formData.get("country")
-  console.log(value);
+  let value = formData.get("country");
 
   fetch(`https://restcountries.com/v3.1/name/${value}`)
       .then((response) => {
@@ -23,10 +26,55 @@ function getCountry(event) {
           return response.json();
         }
       })
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-
+      .then((data) => {
+        let resultCountry = data[0].name.common;
+        output.innerText = resultCountry;
+        resultsList.innerHTML = "";
+        return resultCountry;
+      })
+      .catch((error) => {
+        resultsList.innerHTML = "<li>No result found</li>";
+        console.log(error)});
   inputValue.value = "";
 }
+
+
+// Show search results before submitting
+
+let timeout = 0;
+
+window.addEventListener("load", searchResults);
+
+function searchResults() {
+  inputValue.onkeyup = () => {
+    fetch(`https://restcountries.com/v3.1/name/${inputValue.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.map(result => result.name.common);
+        resultsList.innerHTML = "";
+        
+        clearTimeout(timeout);
+
+        if (inputValue.value.trim().length === 0) {
+          return;
+        }
+
+        timeout = setTimeout(() => {
+          results.forEach(result => {
+            let listItem = document.createElement("li");
+            listItem.innerText = result;
+            listItem.addEventListener("click", function() {
+                inputValue.value = result;
+                resultsList.innerHTML = "";})
+            resultsList.appendChild(listItem);
+          });
+        }, 400);
+        
+      })
+      .catch(() => {
+        resultsList.innerHTML = "<li>No result found</li>";
+      });
+  }
+  }
 
       
