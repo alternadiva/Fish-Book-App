f48d4f3840a6fda547af235d6fda6738b3862529
alternadiva <asomeonelse@gmail.com>
2022-02-14 11:56:15 +0100

    generate a random country

--- script.js
+++ script.js
@@ -5,10 +5,53 @@ let randomBtn = document.getElementById("random-btn");
 let resultsList = document.getElementById("results-list");
 let output = document.getElementById("output");
 
-// Submit input and show result
+// Show search results before submitting
+
+let timeout = 0;
+
+window.addEventListener("load", searchResults);
+
+function searchResults() {
+  inputValue.onkeyup = () => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        resultsList.innerHTML = "";
+        
+        clearTimeout(timeout);
+
+        if (inputValue.value.trim().length === 0) {
+          return;
+        }
+
+        let nameResults = data.flatMap(country => country.name.common);
+        
+        timeout = setTimeout(() => {
+          nameResults.forEach(result => {
+            if (result.toLowerCase().includes(inputValue.value.toLowerCase())) {
+              let listItem = document.createElement("li");
+              listItem.id = "country-list-item";
+              listItem.innerText = result;
+              listItem.addEventListener("click", function() {
+                inputValue.value = result;
+                resultsList.innerHTML = "";})
+              resultsList.appendChild(listItem);
+            }
+          });
+        }, 200);
+      })
+      .catch(() => {
+        resultsList.innerHTML = "<li>No result found</li>";
+      });
+  }
+  }
+
+
+// Submit input and show final result
 
 form.addEventListener("submit", getCountry);
 
+let resultCountry;
 
 function getCountry(event) {
   event.preventDefault();
@@ -27,54 +70,47 @@ function getCountry(event) {
         }
       })
       .then((data) => {
-        let resultCountry = data[0].name.common;
-        output.innerText = resultCountry;
+        resultCountry = data[0].name.common;
+        output.innerHTML = `${resultCountry} 
+                            <img src=${data[0].flags.png} alt="flag of ${resultCountry}" id="flag">`;
         resultsList.innerHTML = "";
         return resultCountry;
       })
       .catch((error) => {
         resultsList.innerHTML = "<li>No result found</li>";
+        output.innerText = "";
         console.log(error)});
   inputValue.value = "";
 }
 
+// Generate a random country
 
-// Show search results before submitting
+randomBtn.addEventListener("click", randomCountry);
 
-let timeout = 0;
+function randomCountry() {
 
-window.addEventListener("load", searchResults);
-
-function searchResults() {
-  inputValue.onkeyup = () => {
-    fetch(`https://restcountries.com/v3.1/name/${inputValue.value}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const results = data.map(result => result.name.common);
-        resultsList.innerHTML = "";
-        
-        clearTimeout(timeout);
-
-        if (inputValue.value.trim().length === 0) {
-          return;
+  fetch("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        if (!response.ok) {
+          const error = new Error(response.status);
+          throw error;
         }
-
-        timeout = setTimeout(() => {
-          results.forEach(result => {
-            let listItem = document.createElement("li");
-            listItem.innerText = result;
-            listItem.addEventListener("click", function() {
-                inputValue.value = result;
-                resultsList.innerHTML = "";})
-            resultsList.appendChild(listItem);
-          });
-        }, 400);
+        else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        let nameResults = data.flatMap(country => country.name.common);
+        let flagResults = data.flatMap(country => country.flags.png);
+        let randomIndex = Math.floor(Math.random() * nameResults.length)
+        resultCountry = nameResults[randomIndex];
+        output.innerHTML = `${resultCountry} 
+                            <img src=${flagResults[randomIndex]} alt="flag of ${resultCountry}" id="flag">`;
+        return resultCountry;
         
       })
-      .catch(() => {
-        resultsList.innerHTML = "<li>No result found</li>";
-      });
-  }
-  }
+      .catch((error) => {
+        console.log(error)});
+}
 
       
